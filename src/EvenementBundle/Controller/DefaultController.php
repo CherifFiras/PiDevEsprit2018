2cf83a2a7a6d16b4a6a1e85c5309dbb5b6b8cbf9
medramy <mohamedrami.hajji.3@esprit.tn>
2018-02-13 19:47:35 +0100

    new file:   composer.phar 	new file:   src/EvenementBundle/Controller/DefaultController.php 	new file:   src/EvenementBundle/EvenementBundle.php 	new file:   src/EvenementBundle/Form/EvenementType.php 	new file:   src/EvenementBundle/Resources/config/routing.yml 	new file:   src/EvenementBundle/Resources/config/services.yml 	new file:   src/EvenementBundle/Resources/views/Default/index.html.twig 	new file:   src/EvenementBundle/Resources/views/Evenement/create.html.twig 	new file:   src/EvenementBundle/Resources/views/Evenement/evenement.html.twig 	renamed:    src/MainBundle/Resources/views/Default/evenement.html.twig -> src/EvenementBundle/Resources/views/Evenement/read.html.twig 	new file:   src/EvenementBundle/Resources/views/Evenement/update.html.twig 	modified:   src/MainBundle/Controller/DefaultController.php 	modified:   src/MainBundle/Entity/Evenement.php 	modified:   src/MainBundle/Resources/config/routing.yml 	new file:   tests/EvenementBundle/Controller/DefaultControllerTest.php

---
+++ src/EvenementBundle/Controller/DefaultController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace EvenementBundle\Controller;
+
+use EvenementBundle\Form\EvenementType;
+use MainBundle\Entity\Evenement;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    public function EvenementAction()
+    {
+        return $this->render('EvenementBundle:Evenement:evenement.html.twig');
+    }
+    public function AjouterEAction(Request $request)
+    {
+
+        $evenement = new Evenement();
+        $form=$this->createForm(EvenementType::class,$evenement);
+        $form->handleRequest($request);
+        if($form->isValid())
+        {
+            $em=$this->getDoctrine()->getManager();
+            $em->persist($evenement);
+            $em->flush();
+            return $this->redirectToRoute("read_evenement");
+        }
+        return $this->render('EvenementBundle:Evenement:create.html.twig', array(
+            "form"=>$form->createView()
+        ));
+    }
+    public function ReadEAction()
+    {
+        $em= $this->getDoctrine()->getManager();
+        $evenements=$em->getRepository("MainBundle:Evenement")->findAll();
+        return $this->render('EvenementBundle:Evenement:read.html.twig', array(
+            "evenements"=>$evenements
+
+        ));
+    }
+    public function UpdateEAction ($id,Request $request)
+    {
+        $em= $this->getDoctrine()->getManager();
+        $evenement=$em->getRepository("MainBundle:Evenement")->find($id);
+        $form=$this->createForm(EvenementType::class,$evenement);
+        $form->handleRequest($request);
+        if($form->isValid())
+        {
+            $em->flush();
+            return $this->redirectToRoute("read_evenement");
+        }
+        return $this->render('EvenementBundle:Evenement:update.html.twig', array(
+            "form"=>$form->createView()
+
+        ));
+    }
+    public function DeleteEAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $evenement= $em->getRepository("MainBundle:Evenement")->find($id);
+        $em->remove($evenement);
+        $em->flush();
+        return $this->redirectToRoute("read_evenement");
+    }
+}
