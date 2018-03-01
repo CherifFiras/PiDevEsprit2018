e8172bb4a9a9f8f83a232b6baeb61498a4d75ce2
CherifFiras <firas.1.2008@hotmail.fr>
2018-03-01 19:14:15 +0100

    modified:   app/AppKernel.php

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,7 +25,7 @@ class AppKernel extends Kernel
             new InteractionBundle\InteractionBundle(),
             new Mgilet\NotificationBundle\MgiletNotificationBundle(),
             new Vich\UploaderBundle\VichUploaderBundle(),
-            new APY\DataGridBundle\APYDataGridBundle(),
+            //new APY\DataGridBundle\APYDataGridBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
